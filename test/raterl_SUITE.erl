df470e7928d053fa8e1fc01e38a2e3c57dd521a7
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-12-03 14:01:35 +0000

    Squash compile-time warning

--- test/raterl_SUITE.erl
+++ test/raterl_SUITE.erl
@@ -18,7 +18,7 @@
 %%
 %% -------------------------------------------------------------------
 -module(raterl_SUITE).
--compile([export_all]).
+-compile([export_all, nowarn_export_all]).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
